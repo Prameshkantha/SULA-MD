3048ebf3274e7117343189711b4f0b2f1499359c
Prameshkantha <kollapd574@gmail.com>
2025-07-11 23:14:27 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝐒𝐔𝐋𝐀-𝐌𝐃=0v9ByKzb#b4hkx1Buut7_OKkk-trJsjvwNO8nj9LkXGVHM0PAqcg' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝐒𝐔𝐋𝐀-𝐌𝐃=Qrd1iQjS#85abl3fSN2oddhfUixpTOdIXjJacLoRA5eqfi5oyfC4' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"false" : process.env.AUTO_READ_STATUS,
 MODE: process.env.MODE === undefined ?"private" : process.env.MODE,
