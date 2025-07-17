94d28898669d78b5f7ac76e859864f25c2549ab0
Prameshkantha <kollapd574@gmail.com>
2025-07-17 15:11:30 +0530

    mage orijinal  settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝐒𝐔𝐋𝐀-𝐌𝐃=Fz1XUJRb#TKywqsHkkCPJdiFzlA1OoMwJqKEv_j2LTulQNK_AyUo' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝐒𝐔𝐋𝐀-𝐌𝐃=Y7EknQJA#6nTc0VcYybUHegjnlb2mwCITrLPLdmnsCCkZqAffjME' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"false" : process.env.AUTO_READ_STATUS,
 MODE: process.env.MODE === undefined ?"private" : process.env.MODE,
